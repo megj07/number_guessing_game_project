4ab2c06e0eecbfefa45a9112c9ecf3f272f8c0ff
meg m <mmcvay89@gmail.com>
2025-01-12 22:40:39 +0000

    feat: Implement game logic and update games_played and best_game after each game

--- number_guess.sh
+++ number_guess.sh
@@ -22,13 +22,13 @@ else
   GAMES_PLAYED=$(echo $USER_INFO | cut -d '|' -f 1 | xargs)
   BEST_GAME=$(echo $USER_INFO | cut -d '|' -f 2 | xargs)
 
-  # If best_game is null, set it to N/A
+  # If best_game is NULL, set it to N/A
   if [[ -z $BEST_GAME || $BEST_GAME == 'NULL' ]]
   then
     BEST_GAME="N/A"
   fi
 
-  # If games_played is null, set it to 0
+  # If games_played is NULL, set it to 0
   if [[ -z $GAMES_PLAYED || $GAMES_PLAYED == 'NULL' ]]
   then
     GAMES_PLAYED="0"
@@ -36,3 +36,39 @@ else
 
   echo "Welcome back, $username! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+# Game logic - randomly generate a secret number
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+GUESS_COUNT=0
+
+# Game loop for guessing
+echo "Guess the secret number between 1 and 1000:"
+while true
+do
+  read guess
+  ((GUESS_COUNT++))
+
+  if ! [[ "$guess" =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+    elif [[ "$guess" -gt "$SECRET_NUMBER" ]]
+    then
+      echo "It's lower than that, guess again:"
+    elif [[ "$guess" -lt "$SECRET_NUMBER" ]]
+    then
+      echo "It's higher than that, guess again:"
+    else
+      echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
+      break
+    fi
+  done
+
+  # After game, update database
+  # Increment games_played and update best_game if needed
+  if [[ "$BEST_GAME" == "N/A" || "$GUESS_COUNT" -lt "$BEST_GAME" ]]
+  then
+    BEST_GAME=$GUESS_COUNT
+  fi
+
+  # Update the games_played and best_game in database
+  $PSQL "UPDATE users SET games_played = games_played + 1, best_game = $BEST_GAME WHERE username = '$username'"
