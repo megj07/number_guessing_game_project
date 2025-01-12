21b080c5139a40477c9c3493ddcd74db0c13045b
meg m <mmcvay89@gmail.com>
2025-01-12 21:55:41 +0000

    feat: Add logic to check if username exists and display welcome message

--- number_guess.sh
+++ number_guess.sh
@@ -7,3 +7,17 @@ read username
 # PSQL command
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+# Check if username exists in database
+USER_INFO=$($PSQL "SELECT games_played, best_game FROM users WHERE username = '$username'")
+
+if [[ -z $USER_INFO ]]
+then
+  # If username doesn't exist
+  echo "Welcome, $username! It looks like this is your first time here."
+  $PSQL "INSERT INTO users (username) VALUES ('$username')"
+else
+  # If username exists
+  GAMES_PLAYED=$(echo $USER_INFO | cut -d '|' -f 1 | xargs)
+  BEST_GAME=$(echo $USER_INFO | cut -d '|' -f 2 | xargs)
+  echo "Welcome back, $username! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
