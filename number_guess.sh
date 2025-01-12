ab704ebbbb41b3d6b7a26c14aa1b010a56daf75d
meg m <mmcvay89@gmail.com>
2025-01-12 22:17:51 +0000

    fix: Handle NULL values in database properly and adjust output for best_game and games_played

--- number_guess.sh
+++ number_guess.sh
@@ -15,9 +15,24 @@ then
   # If username doesn't exist
   echo "Welcome, $username! It looks like this is your first time here."
   $PSQL "INSERT INTO users (username) VALUES ('$username')"
+  GAMES_PLAYED=0
+  BEST_GAME="N/A"
 else
   # If username exists
   GAMES_PLAYED=$(echo $USER_INFO | cut -d '|' -f 1 | xargs)
   BEST_GAME=$(echo $USER_INFO | cut -d '|' -f 2 | xargs)
+
+  # If best_game is null, set it to N/A
+  if [[ -z $BEST_GAME || $BEST_GAME == 'NULL' ]]
+  then
+    BEST_GAME="N/A"
+  fi
+
+  # If games_played is null, set it to 0
+  if [[ -z $GAMES_PLAYED || $GAMES_PLAYED == 'NULL' ]]
+  then
+    GAMES_PLAYED="0"
+  fi
+
   echo "Welcome back, $username! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
